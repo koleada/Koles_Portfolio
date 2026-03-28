d304d409bd0b575cabc1b20164f0358dcb9edbf9
kole <koleniz.da@gmail.com>
2026-03-28 15:57:34 -0400

    no jekyll

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("styles");
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("scripts");
-
     eleventyConfig.addCollection("posts", function (collectionApi) {
         return collectionApi.getFilteredByGlob("writeups/posts/*.md").reverse();
     });
